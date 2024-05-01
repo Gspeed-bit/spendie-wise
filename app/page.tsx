a67aef11957e2ad000a0ef0181aca6c26605061f
Adeyeye Ayodeji Daniel <adeyeye17@gmail.com>
2024-05-02 01:06:46 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image src="/logo.png" width={100} height={50} alt="Logo" />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam,
-        molestias! Nobis nemo aliquid necessitatibus ad doloribus eligendi et
-        itaque quos neque nesciunt est asperiores, vitae libero distinctio.
-        Ratione, natus vel!
-      </p>
+      
     </main>
   );
 }

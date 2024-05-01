dad3ab395a5fde2612fb236316ab4af4090bf688
Adeyeye Ayodeji Daniel <adeyeye17@gmail.com>
2024-05-02 01:06:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src="/image copy.png" width={100} height={50} alt="Logo" />
+      <Image src="/logo.png" width={100} height={50} alt="Logo" />
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam,
         molestias! Nobis nemo aliquid necessitatibus ad doloribus eligendi et

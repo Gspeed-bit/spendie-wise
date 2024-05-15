88e7706d2eff16252f9432ede130ad59b278efca
Adeyeye Ayodeji Daniel <58330087+Gspeed-bit@users.noreply.github.com>
2024-05-15 20:01:34 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default {
   driver: "pg",
   dbCredentials: {
     connectionString:
-      "postgresql://Spendiedb_owner:7t8xImGYsSWA@ep-lingering-thunder-a5kcfrdz.us-east-2.aws.neon.tech/Spendiedb?sslmode=require",
-    // connectionString: (process.env.NEXT_PUBLIC_DATABASE_URL!)
+    
+    connectionString: (process.env.NEXT_PUBLIC_DATABASE_URL!)
   },
 } satisfies Config;
